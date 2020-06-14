8afa7f66d56bfd1fff17bb857991cb2181ce287d
Angga Lesmana <56834131+AnggaChelsea@users.noreply.github.com>
2020-06-15 06:36:33 +0700

    Add files via upload

---
+++ template/v_footer.php
@@ -0,0 +1,10 @@
+
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2020 <div class="bullet"></div> Design By <a href="https://github.com/AnggaChelsea">Angga Lesmana</a>
+        </div>
+        <div class="footer-right">
+          
+        </div>
+      </footer>
+</div>
---
+++ template/v_nav.php
@@ -0,0 +1,38 @@
+
+      <div class="main-sidebar sidebar-style-2">
+        <aside id="sidebar-wrapper">
+          <div class="sidebar-brand">
+            <a href="index.html">Bappeda Sukabumi</a>
+          </div>
+          <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+          </div>
+          <ul class="sidebar-menu">
+            <li class="menu-header">Menu Utama</li>
+            <li class=active><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Menu Utama</span></a></li>
+            <li class=active><a class="nav-link" href="<?php echo base_url('home/marker') ?>"><i class="fa fa-marker"></i> <span>Marker</span></a></li>
+            <li class="dropdown">
+              <a href="<?php base_url('home') ?>"><i class="fa fa-map"></i> <span>View Map</span></a>
+            </li>
+            <li class=active><a class="nav-link" href="<?php base_url() ?>./template/blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            
+          <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
+            <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
+              <i class="fa fa-globe-asia"></i>Bappeda street
+            </a>
+          </div>        </aside>
+      </div>
+</div>
+
+<div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Pemetaan Kegiatan Bappeda Sukabumi</h1>
+            <p><?php $title ?></p>
+          </div>
+
+          <div class="section-body">
+          </div>
+        </section>
+      </div>
+</div>
